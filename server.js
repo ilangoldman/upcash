b26940e2ea6ee0ace800fbfc58459dac1d387fd5
ilangoldman <ilan.goldman.ig@gmail.com>
2018-05-21 20:46:12 -0300

    deploy gcs

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// [START app]
 if (process.env.NODE_ENV === "production") {
     require("@google/cloud-trace").start();
 }
@@ -5,3 +8,21 @@ if (process.env.NODE_ENV === "production") {
 if (process.env.GCLOUD_PROJECT) {
     require("@google/cloud-debug").start();
 }
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
+// [END app]
+
+
